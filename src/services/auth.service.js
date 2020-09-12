89487a988e3956f1de47a83c070d7b75c63c6e20
fayssaleo <fayssal.ourezzouq@gmail.com>
2020-09-12 18:41:45 +0100

    Revert "Merge branch 'master' of https://github.com/sanaaen/E-Coffee_front"
    This reverts commit 488a68e30879b1607dada3a588419300827999e9, reversing
    changes made to cdec5e8d3a995b6f435c6337800395f7a01e5273.

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from "axios";
 
 const API_URL = "/";
@@ -36,43 +35,4 @@ class AuthService {
   }
 }
 
-=======
-import axios from "axios";
-
-const API_URL = "/";
-
-class AuthService {
-  login(username, password) {
-    return axios
-      .post(API_URL + "login", {
-        username,
-        password
-      })
-      .then(response => {
-        if (response.data.accessToken) {
-          localStorage.setItem("user", JSON.stringify(response.data));
-        }
-
-        return response.data;
-      });
-  }
-
-  logout() {
-    localStorage.removeItem("user");
-  }
-
-  register(username, email, password) {
-    return axios.post(API_URL + "register", {
-      username,
-      email,
-      password
-    });
-  }
-
-  getCurrentUser() {
-    return JSON.parse(localStorage.getItem('user'));;
-  }
-}
-
->>>>>>> 7f3007c919d03d4d348fe35cb0144c5d64a0938e
 export default new AuthService();
