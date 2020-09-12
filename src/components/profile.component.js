701f6361a164b5310bf8309e9e79ede4f95433ba
fayssaleo <fayssal.ourezzouq@gmail.com>
2020-09-12 23:06:45 +0100

    profile.co

--- src/components/profile.component.js
+++ src/components/profile.component.js
@@ -280,58 +280,5 @@ class Profile extends Component {
       </>
     );
   }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import React, { Component } from "react";
-import AuthService from "../services/auth.service";
-
-export default class Profile extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentUser: AuthService.getCurrentUser()
-    };
-  }
-
-  render() {
-    const { currentUser } = this.state;
-
-    return (
-      <div className="container">
-        <header className="jumbotron">
-          <h3>
-            <strong>{currentUser.username}</strong> Profile
-          </h3>
-        </header>
-        <p>
-          <strong>Token:</strong>{" "}
-          {currentUser.accessToken.substring(0, 20)} ...{" "}
-          {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-        </p>
-        <p>
-          <strong>Id:</strong>{" "}
-          {currentUser.id}
-        </p>
-        <p>
-          <strong>Email:</strong>{" "}
-          {currentUser.email}
-        </p>
-        <strong>Authorities:</strong>
-        <ul>
-          {currentUser.roles &&
-            currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-        </ul>
-      </div>
-    );
-  }
->>>>>>> 7f3007c919d03d4d348fe35cb0144c5d64a0938e
-}
-=======
 }
 export default withStyles(styles, { withTheme: true })(Profile);
->>>>>>> dashbordUserManager
-=======
-}
->>>>>>> 89487a988e3956f1de47a83c070d7b75c63c6e20
